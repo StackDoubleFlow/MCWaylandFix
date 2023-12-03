b7b01187c1d26a50f981e7b9a57216fd1f13f271
StackDoubleFlow <ojaslandge@gmail.com>
2023-12-03 01:12:20 -0600

    Initial commit

---
+++ src/main/java/net/stackdoubleflow/waylandfix/WaylandFix.java
@@ -0,0 +1,8 @@
+package net.stackdoubleflow.waylandfix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WaylandFix implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
