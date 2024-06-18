18ddc1ab8381edd3432362e156e6d174db865e02
StackDoubleFlow <ojaslandge@gmail.com>
2024-06-17 21:43:21 -0700

    Remove fabric-api dependency and allow all minecraft versions

--- src/main/java/net/stackdoubleflow/waylandfix/WaylandFix.java
+++
@@ -1,8 +0,0 @@
-package net.stackdoubleflow.waylandfix;
-
-import net.fabricmc.api.ModInitializer;
-
-public class WaylandFix implements ModInitializer {
-    @Override
-    public void onInitialize() {}
-}
